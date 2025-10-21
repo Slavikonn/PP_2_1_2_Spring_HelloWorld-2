c47baf73b21a251d5485709b2e05be1be2d10e79
Slavikonn <slavikon132@gmail.com>
2025-10-21 23:25:10 +0300

    add Pojo class and implement Spring bean methods

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,24 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
+
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        Cat catBean2 = (Cat) applicationContext.getBean("cat");
+
+
         System.out.println(bean.getMessage());
+        System.out.println(bean2.getMessage());
+        System.out.println(bean == bean2);
+
+        System.out.println();
+
+        System.out.println(catBean.getMessage());
+        System.out.println(catBean2.getMessage());
+        System.out.println(catBean == catBean2);
     }
 }
